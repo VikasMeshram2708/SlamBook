2a0254e64a3280a1b4f1a9696c5d02353d68ed56
VikasMeshram2708 <meshramvikas744@gmail.com>
2022-12-27 21:32:12 +0530

    :tada:creatd login route with token authentication

--- Routes/auth.js
+++ Routes/auth.js
@@ -10,7 +10,9 @@ const User = db.get("users");
 
 const bcrypt = require("bcryptjs");
 
-// Route 1: Create a User using post : "/api/auth/createUser"
+const jwt = require("jsonwebtoken");
+
+// Route 1: Create a User using POST : "/api/auth/createUser"
 router.post("/createUser", async (req, res) => {
   try {
     // validate the user
@@ -31,11 +33,53 @@ router.post("/createUser", async (req, res) => {
       const createdUser = await User.insert(user);
       return res.status(201).json({
         message: "User Registered Successfully",
-        data: user,
+        data: createdUser,
       });
     }
     return res.status(422).json({
       message: "Try to register with valid Credentials",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Some Internal Server Error",
+      error: error.message,
+    });
+  }
+});
+
+// Route 2 : User login using POST : "/api/auth/userLogin"
+router.post("/userLogin", async (req, res) => {
+  try {
+    // find the user
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (user) {
+      // compare the password
+      const isValidKey = await bcrypt.compare(req.body.password, user.password);
+      if (!isValidKey) {
+        return res.status(404).json({
+          message:
+            "Hey, try to login with valid credentails invalid key provided",
+        });
+      }
+      // show success response and sign jwt
+      const data = {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      };
+
+      const authToken = jwt.sign(data, process.env.JWT_SECRET);
+
+      return res.status(201).json({
+        message: "User Registered Successfully",
+        token: authToken,
+      });
+    }
+    return res.status(422).json({
+      message: "Try to login with valid Credentials",
     });
   } catch (error) {
     return res.status(500).json({
